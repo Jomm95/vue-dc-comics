d1458b4600f67a84fd2c4f64da0a0fdfb67bad35
Thomas <venturini.thomas95@gmail.com>
2022-03-02 14:33:39 +0100

    inizializzazione

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>My Footer</h1>
+  </div>
+    
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+  
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>My Main</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyMain",
+  
+};
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
