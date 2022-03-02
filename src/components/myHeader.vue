8bae6eabfe2d80de8e5c3fa23ad5a07268cdae24
Thomas <venturini.thomas95@gmail.com>
2022-03-02 16:34:40 +0100

    .

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,8 +1,49 @@
 <template>
-    <div>
-        <h1>MyHeader</h1>
+    <header>
+        <div>
+            <nav>
+
+                <a href=''>
+                    <img class="logo" src='../assets/img/dc-logo.png'>
+                </a>
+
+                <ul>
+                    <li>
+                        <a href='#'>CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href='#'>COMICS</a>
+                    </li>
+                    <li>
+                        <a href='#'>MOVIES</a>
+                    </li>
+                    <li>
+                        <a href='#'>TV</a>
+                    </li>
+                    <li>
+                        <a href='#'>GAMES</a>
+                    </li>
+                    <li>
+                        <a href='#'>COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href='#'>VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href='#'>FANS</a>
+                    </li>
+                    <li>
+                        <a href='#'>NEWS</a>
+                    </li>
+                    <li>
+                        <a href='#'>SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
         
-    </div>
+    </header>
 </template>
     
 <script>
@@ -12,5 +53,52 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+@import '../assets/variables.scss';
+
+header {
+    text-align: center;
+}
+
+nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top:10px;
+    height:90px;
+}
+
+.logo {
+    width: 60%;
+    height: 60%;
+}
+
+ul {
+    display:flex;
+    justify-content: center;
+    font-size: 12px;
+    list-style: none;
+    margin-left: 180px;
+
+    li {
+    margin:0px 12px;
+    display:inline-block;
+    height:90px;
+    line-height: 90px;
+    border-bottom: 3px solid transparent;
+
+      a {
+        text-decoration: none;
+        color:black;
+        font-weight: bold;
+        }
+    }
+}
+
+li:hover{
+    cursor: pointer;
+    color:#0282f9;
+    border-bottom: 4px solid $primaryColor;
+}
 
 </style>
